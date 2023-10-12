9746a24d69e683d51a7553eef26d0c59e7f82be4
radoslawszmit@gmail.com <radoslawszmit@gmail.com>
2023-10-12 21:09:09 +0000

    Movielens SQL

---
+++ databricks/movielens/MovielensSQL.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+show databases;
+
+-- COMMAND ----------
+
+use movielens;
+
+-- COMMAND ----------
+
+show tables;
+
+-- COMMAND ----------
+
+describe extended movies;
+
+-- COMMAND ----------
+
+select * from movies limit 10;
